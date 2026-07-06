24f70d4e174e8b11f346ec6b0b8d0c4dec783790
jiangming <837639305@qq.com>
2026-07-06 14:51:28 +0800

    chore: 移除 node_modules 和 dist 版本追踪

--- web/dist/assets/LoginPage-DYLlqAOk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-CyeQP79T.js";const o={};function r(t,a){return c(),n("div",null,"登录页面")}const _=e(o,[["render",r]]);export{_ as default};
--- web/dist/assets/MainPage-B5d_GphL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-CyeQP79T.js";const o={};function r(t,a){return c(),n("div",null,"主页")}const _=e(o,[["render",r]]);export{_ as default};
